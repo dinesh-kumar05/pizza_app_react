831779a33aaf58a0b7cc10021c70053c06e9ae03
dinesh-kumar05 <dineshsrinivasan05.ds@gmail.com>
2023-10-18 22:54:25 +0530

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+  const [product, setProduct] = useState([]);
+
+  const getDashboardData = async () => {
+    const dashData = await axios.get("http://localhost:3003/dashboard");
+    setData(dashData.data.message);
+  };
+
+  const getServiceData = async () => {
+    const serviceData = await axios.get("http://localhost:3003/service");
+    setData(serviceData.data.message);
+  };
+
+  const getProduct = async () => {
+    const productData = await axios.get("http://localhost:3003/products");
+    setProduct(productData.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getDashboardData}>Get Dashboard Data</button>
+      <button onClick={getServiceData}>Get Service Data</button>
+      <button onClick={getProduct}>Get Product Data</button>
+      <br/>
+      {data}
+      <br/>
+      <ul>
+        {
+          product.map((item) => {
+            return <li> {item.name} - {item.price}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
